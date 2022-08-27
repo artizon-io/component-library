52f10eb6aa0c9bdc83ebddaba33be2d3b1d78b0b
Ming Sze <mingsum.sam@gmail.com>
2022-08-27 13:27:46 +0800

    build: cleanup tsconfigs & NPM scripts

--- rollup.config.js
+++ rollup.config.js
@@ -33,10 +33,11 @@ export default {
       // See
       // https://stackoverflow.com/questions/51970359/resolve-woff-imports-with-rollup
       // by default, rollup-plugin-url will not handle font files
-      include: ['**/*.woff', '**/*.woff2'],
+      include: ['**/*.woff', '**/*.woff2', '**/*.webp', '**/*.jpg'],
       // setting infinite limit will ensure that the files 
-      // are always bundled with the code, not copied to /dist
+      // are always bundled with the code (inline), not copied to /dist
       limit: Infinity,
+      // publicPath: 'assets'
     })
   ],
   external: [
